d65cfc843f81f556dfac7667387f203cf400436a
Synkevych Roman <sunkevu4@gmail.com>
2019-06-21 12:02:33 +0300

    Now it's work great, let's merge this branch to master!

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,9 @@
 const regexps = {
-  name: '[a-zA-Z\\s]{2,500}',
-  email: '[\\da-zA-Z\\@\\.]{5,500}',
+  name: '[a-zA-Z\\s]{1,500}',
+  email: '[^@]+@[^.]+..+',
   phone_number: '^[\\d]{10,12}',
-  address: '[\\W\\w\\d\\s]{0,500}',
-  about_me: '[\\W\\w\\d\\s]{0,500}'
+  address: '[\\W\\w\\d\\s]{1,500}',
+  about_me: '[\\W\\w\\d\\s]{1,500}'
 };
 const forms = document.getElementById('form1');
 const formsElement = document.getElementsByClassName('form-control');
@@ -32,37 +32,34 @@ for (let i = 0; i < forms.length; i++) {
   forms[i].setAttribute('novalidate', true);
 }
 
-// Listen to all blur events
 document.addEventListener(
   'blur',
   function(event) {
-    console.log(event);
-    event.target.classList.remove('error');
+    setTimeout(()=>{event.target.classList.remove('error')}, 1000);
   },
   true
 );
 
 function formChecker(elem) {
   for (let i = 0; i < elem.length - 1; i++) {
-   // elem[i].classList.remove('error');
     let nameElem = elem[i].name;
+
     if (elem[i].tagName === 'INPUT' || elem[i].tagName === 'TEXTAREA') {
       let pattern = new RegExp(regexps[nameElem]);
       let isValid = elem[i].value.match(pattern);
 
       if (isValid !== null && isValid[0] == isValid.input) {
-        console.log('valid', pattern, nameElem, isValid);
         formData[nameElem] = elem[i].value;
       } else {
-        console.log('not valid', nameElem, isValid);
-        elem[i].classList.add('error');
-        elem[i].focus();
-        return false;
+        if (!(nameElem == 'address' || (nameElem == 'about_me' &&  elem[i].value == ""))) {
+          elem[i].classList.add('error');
+          elem[i].focus();
+          return false;
+        }
       }
     }
     if (elem[i].tagName === 'SELECT') {
       let selectValue = elem[i][elem[i].selectedIndex].text;
-      console.log(elem[i][elem[i].selectedIndex].text);
 
       if (elem[i][elem[i].selectedIndex].value > 0) {
         formData[nameElem] = selectValue;
@@ -80,23 +77,10 @@ function formChecker(elem) {
 
 submit[0].addEventListener('click', function(evt) {
   evt.preventDefault();
-  if (!!formChecker(formsElement)){
-  //  sendUsers(formChecker(formsElement), apiUrl + 'users');
+  if (!!formChecker(formsElement)) {
+    sendUsers(formChecker(formsElement), apiUrl + 'users');
   }
-  console.log(formData);
 });
-
-function renderElement(tag, value, id) {
-  const tagName = document.getElementById('' + tag);
-  tagName.innerHTML += `<option value=${id}>${value}</option>`;
-}
-
-function renderResult(value,message){
-    const tagName = document.getElementById('container');
-    tagName.innerHTML += `<div class="result-message">${value}
-    ${message}
-    </div>`;
-}
 
 function getStates(tagName) {
   const oReq = new XMLHttpRequest();
@@ -106,8 +90,9 @@ function getStates(tagName) {
 
   function reqListener() {
     if (oReq.status !== 200) {
-      console.log('Error ', oReq.status + ': ' + oReq.statusText);
-      renderResult("Error");
+      alert(
+        'Sorry 😔, we had difficulties with uploading data from server, try again later 😉.'
+      );
     } else {
       // вывести результат
       lacation[tagName] = JSON.parse(this.responseText);
@@ -124,23 +109,25 @@ getStates('states');
 getStates('countries');
 getStates('cities');
 getStates('users');
-console.log(lacation);
 
 function sendUsers(user, url) {
-  console.log(user, url);
   var json = JSON.stringify(user);
 
   var xhr = new XMLHttpRequest();
   xhr.open('POST', url, true);
   xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
   xhr.onload = function() {
-    var users = JSON.parse(xhr.responseText);
-    if (xhr.readyState == 4 && xhr.status == '200') {
-      console.table(users);
-    } else {
-      console.error(users);
+    if (xhr.readyState !== 4 && xhr.status !== '200') {
+      alert(
+        'Sorry 😔, we had difficulties with uploading data to the server, try again later ... 😉'
+      );
     }
   };
   xhr.send(json);
-  getStates('users'+user.id);
+  alert('Your data has been sent!');
+}
+
+function renderElement(tag, value, id) {
+  const tagName = document.getElementById('' + tag);
+  tagName.innerHTML += `<option value=${id}>${value}</option>`;
 }
